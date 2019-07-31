342e89d40fb8e56c4404c67403c23c135fb88dde
kevinbece5 <kevinbece5@gmail.com>
2019-07-31 16:49:55 -0700

    init for react set up

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            // Logo will go here
+            // booking component will go here and will handle modal 
+            // list of bookings will go here 
+            <Header />
+        )
+    }
+}
