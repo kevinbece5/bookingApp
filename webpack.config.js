49e8f3494a04a020231d08a585cc73236ef7126d
kevinbece5 <kevinbece5@gmail.com>
2019-07-31 16:48:57 -0700

    init for repo

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+module.exports = {
+  entry: './src/index.js',
+  //output of the bundle
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  //this will handle all the react code to be bundled right
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  //hot reloafer
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  //When running npm start the output of dev server will be to this folder
+  devServer: {
+    contentBase: './dist'
+  }
+};
