eed48646973a7dd20b1da0c71cdd606482d533f3
kevinbece5 <kevinbece5@gmail.com>
2019-07-31 16:59:08 -0700

    styles for header

---
+++ src/Header/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: end;
+    background-color: #FFF
+    padding-bottom: 10px;
+`;
+
+export const Logo = styled.div`
+    width: 10%;
+    padding-left: 5px;
+`;
+
