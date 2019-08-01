effc16ca4c486107689260275bedb3be9c09c7aa
kevinbece5 <kevinbece5@gmail.com>
2019-07-31 17:01:01 -0700

    changed header component to from class to stateless

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -3,22 +3,12 @@ import * as Styles from './style';
 import Logo from '../assets/logo.jsx'
 
 
+export const Header = () => (
+    <Styles.Container>
+        <Styles.Logo>
+            <Logo />
+        </Styles.Logo>
+    </Styles.Container>
+)
 
-
-export default class Header extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            //Conatiner
-            //IMG tag for logo
-            <Styles.Container>
-                <Styles.Logo>
-                    <Logo />
-                </Styles.Logo>
-            </Styles.Container>
-        )
-    }
-}
+export default Header
