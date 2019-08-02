1e0906f8480d796b2dae748a4c9be56648a6758f
kevinbece5 <kevinbece5@gmail.com>
2019-08-01 22:58:15 -0700

    added Bookings comonent to app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import Booker from './Booker';
 
 export default class App extends React.Component {
     constructor(props) {
@@ -11,7 +12,10 @@ export default class App extends React.Component {
             // Logo will go here
             // booking component will go here and will handle modal 
             // list of bookings will go here 
-            <Header />
+            <div style={{ backgroundColor: "rgb(207,207,207)" }} >
+                <Header />
+                <Booker />
+            </div>
         )
     }
 }
