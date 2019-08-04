59bc72888d9b106cc7c3323d9283d2c7f86d04e2
kevinbece5 <kevinbece5@gmail.com>
2019-08-03 21:57:40 -0700

    imported components into app and removed hotloading

--- src/app.js
+++ src/app.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import Header from './Header';
 import Booker from './Booker';
+import Bookings from './Bookings';
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            updateBookings: false,
+        }
     }
+
+    updateBookings = () => this.setState({ updateBookings: !this.state.updateBookings });
 
     render() {
         return (
             // Logo will go here
             // booking component will go here and will handle modal 
             // list of bookings will go here 
-            <div style={{ backgroundColor: "rgb(207,207,207)" }} >
+            <div >
                 <Header />
-                <Booker />
+                <Booker updateBookings={this.updateBookings} />
+                <Bookings updateBookings={this.state.updateBookings} bookingsUpdated={this.updateBookings} />
             </div>
         )
     }
