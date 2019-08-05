2135dcf0890a1447b54f3f4b17186e99cca74b72
kevinbece5 <kevinbece5@gmail.com>
2019-08-04 17:40:34 -0700

    filter logic inside booker component

--- src/Booker/index.jsx
+++ src/Booker/index.jsx
@@ -7,10 +7,19 @@ export default class Booker extends React.Component {
         super(props);
         this.state = {
             toggleModal: false,
+            filter: 'all'
         }
     };
 
     toggleModal = () => this.setState({ toggleModal: !this.state.toggleModal });
+
+    updateItem = (event) => {
+        let value = event.target.value;
+        let name = event.target.name;
+        this.setState({ [name]: value }, () => {
+            this.props.updateFilter(this.state.filter);
+        });
+    }
 
     render() {
         return (
@@ -18,6 +27,14 @@ export default class Booker extends React.Component {
                 <Styles.Header>
                     Bookings
                 </Styles.Header>
+                <Styles.ButtonContainer>
+                    <Styles.ItemHeader>Filter by Booking Type</Styles.ItemHeader>
+                    <Styles.Select name='filter' value={this.state.filter} id="type-select" onChange={this.updateItem}>
+                        <Styles.Option name='filter' value="all">All</Styles.Option>
+                        <Styles.Option name='filter' value="houseKeeping">Housekeeping</Styles.Option>
+                        <Styles.Option name='filter' value="dogWalking">Dog Walking</Styles.Option>
+                    </Styles.Select>
+                </Styles.ButtonContainer>
                 <Styles.ButtonContainer>
                     <Styles.BookingsBtn onClick={this.toggleModal}>
                         Create Booking
